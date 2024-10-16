a898affa7efef04a782e962a0b055fe1a83c4662
Elizaveta Berdnikova <liza.berdnikova@gmail.com>
2024-10-16 22:38:20 +0700

    жаба

---
+++ src/main/java/Book.java
@@ -0,0 +1,14 @@
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Book {
+    private String name;
+    private String author;
+    private Integer publishingYear;
+    private String isbn;
+    private String publisher;
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,36 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Gson gson = new Gson();
+        String json = Files.readString(Path.of("src\\main\\resources\\books.json"));
+        Type type = new TypeToken<List<Visitor>>(){}.getType();
+        List<Visitor> listVisitors = gson.fromJson(json, type);
+
+        //task 1
+        System.out.println(listVisitors.size());
+
+        //task 2
+        HashSet<Book> books = new HashSet<>();
+        for (Visitor visitor : listVisitors) {
+            books.addAll(visitor.getFavoriteBooks());
+        }
+        System.out.println(books);
+        System.out.println(books.size());
+
+        //task 3
+        System.out.println(books.stream().sorted(Comparator.comparing(Book::getPublishingYear)).toList());
+
     }
 }
